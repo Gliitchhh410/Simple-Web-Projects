00bd044c08fecfe6aec4b5e683794ece64e22d01
Gliitchhh410 <ahmedmounir532@gmail.com>
2025-12-20 21:28:04 +0200

    fix: next()

--- Project 1 - Better/src/models/quizEngine.js
+++ Project 1 - Better/src/models/quizEngine.js
@@ -1,43 +1,38 @@
 export class QuizEngine {
-    constructor(questions){
-        this.questions = questions
-        this.score = 0
-        this.currentIndex = 0
+  constructor(questions) {
+    this.questions = questions;
+    this.score = 0;
+    this.currentIndex = 0;
+  }
+
+  get totalQuestions() {
+    return this.questions.length;
+  }
+  get currentQuestion() {
+    return this.questions[this.currentIndex];
+  }
+
+  get isEnded() {
+    return this.currentIndex >= this.questions.length;
+  }
+
+  get progressPercent() {
+    return (this.currentIndex / this.questions.length) * 100;
+  }
+
+  submitAnswer(isCorrect) {
+    if (isCorrect) {
+      this.score++;
     }
+    return isCorrect;
+  }
 
+  next() {
+    this.currentIndex++;
+  }
 
-    get totalQuestions(){
-        return this.questions.length
-    }
-    get currentQuestion(){
-        return this.questions[this.currentIndex]
-    }
-
-
-    get isEnded(){
-        return this.currentIndex >= this.questions.length
-    }
-
-
-    get progressPercent(){
-        return (this.currentIndex / this.questions.length) * 100
-    }
-
-
-    submitAnswer(isCorrect){
-        if (isCorrect){
-            this.score++
-        }
-        return isCorrect
-    }
-
-    next(){
-        this.index++
-    }
-
-
-    restart(){
-        this.score = 0
-        this.currentIndex = 0
-    }
+  restart() {
+    this.score = 0;
+    this.currentIndex = 0;
+  }
 }
