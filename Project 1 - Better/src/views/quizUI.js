dd7d63a5e2d52e7f59baf3facca15c798a31f98c
Gliitchhh410 <ahmedmounir532@gmail.com>
2025-12-20 20:58:29 +0200

    fix: QuizUI constructor

--- Project 1 - Better/src/views/quizUI.js
+++ Project 1 - Better/src/views/quizUI.js
@@ -1,7 +1,16 @@
 export class QuizUI{
     constructor(engine, domElements){
         this.engine = engine
-        this.domElements = domElements
+        this.dom = domElements
         this.isLocked = false
+
+
+        this.dom.totalQuestionsSpan.textContent = this.engine.totalQuestions
+        this.dom.maxScoreSpan.textContent = this.engine.totalQuestions
+
+
+        this.bindEvents()
     }
+
+
 }
