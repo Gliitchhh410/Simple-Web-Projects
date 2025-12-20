2d7fcda1a78cff7580c4894080a4c441b0b28ae2
Gliitchhh410 <ahmedmounir532@gmail.com>
2025-12-20 21:34:19 +0200

    sytntical fixes all over the place

--- Project 1 - Better/src/views/quizUI.js
+++ Project 1 - Better/src/views/quizUI.js
@@ -1,122 +1,105 @@
-export class QuizUI{
-    constructor(engine, domElements){
-        this.engine = engine
-        this.dom = domElements
-        this.isLocked = false
+export class QuizUI {
+  constructor(engine, domElements) {
+    this.engine = engine;
+    this.dom = domElements;
+    this.isLocked = false;
 
+    this.dom.totalQuestionsSpan.textContent = this.engine.totalQuestions;
+    this.dom.maxScoreSpan.textContent = this.engine.totalQuestions;
 
-        this.dom.totalQuestionsSpan.textContent = this.engine.totalQuestions
-        this.dom.maxScoreSpan.textContent = this.engine.totalQuestions
+    this.bindEvents();
+  }
 
+  bindEvents() {
+    this.dom.startButton.addEventListener("click", () => this.start());
+    this.dom.restartButton.addEventListener("click", () => this.restart());
+  }
 
-        this.bindEvents()
+  start() {
+    this.dom.startScreen.classList.remove("active");
+    this.dom.quizScreen.classList.add("active");
+    this.dom.resultScreen.classList.remove("active");
+
+    this.dom.scoreSpan.textContent = 0;
+    this.renderQuestion();
+  }
+
+  restart() {
+    this.engine.restart();
+    this.start();
+  }
+
+  renderQuestion() {
+    this.isLocked = false;
+    const currentQ = this.engine.currentQuestion;
+
+    this.dom.currentQuestionSpan.textContent = this.engine.currentIndex + 1;
+    this.dom.question.textContent = currentQ.question;
+    this.dom.progressBar.style.width = `${this.engine.progressPercent}%`;
+
+    this.dom.answersContainer.innerHTML = "";
+
+    currentQ.answers.forEach((answer) => {
+      const button = document.createElement("button");
+      button.textContent = answer.text;
+      button.classList.add("answer-btn");
+      button.dataset.correct = answer.correct;
+
+      button.addEventListener("click", (e) => {
+        this.handleAnswer(e);
+      });
+      this.dom.answersContainer.appendChild(button);
+    });
+  }
+
+  handleAnswer(event) {
+    if (this.isLocked) return;
+    this.isLocked = true;
+
+    const selectedButton = event.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+
+    this.engine.submitAnswer(isCorrect);
+
+    this.highlightAnswer(selectedButton);
+
+    if (isCorrect) {
+      this.dom.scoreSpan.textContent = this.engine.score;
     }
 
+    setTimeout(() => {
+      this.engine.next();
+      if (this.engine.isEnded) {
+        this.showResults();
+      } else {
+        this.renderQuestion();
+      }
+    }, 500);
+  }
 
-    bindEvents(){
-        this.dom.startButton.addEventListener("click", ()=> this.start())
-        this.dom.restartButton.addEventListener("click", ()=> this.restart())
-    }
+  highlightAnswer() {
+    Array.from(this.dom.answersContainer.children).forEach((button) => {
+      if (button.dataset.correct === "true") {
+        button.classList.add("correct");
+      } else if (button === selectedButton) {
+        button.classList.add("incorrect");
+      }
+    });
+  }
 
-    start(){
-        this.dom.startScreen.classList.remove("active")
-        this.dom.quizScreen.classList.add("active")
-        this.dom.resultScreen.classList.remove("active")
+  showResults() {
+    this.dom.quizScreen.classList.remove("active");
+    this.dom.startScreen.classList.remove("active");
+    this.dom.resultScreen.classList.add("active");
 
-        this.dom.scoreSpan.textContent =    0
-        this.renderQuestion()
-    }
+    this.dom.finalScoreSpan.textContent = this.engine.score;
 
+    const percentage = (this.engine.score / this.engine.totalQuestions) * 100;
 
-    restart(){
-        this.engine.start()
-        this.start()
-    }
-
-
-    renderQuestion(){
-        this.isLocked = false
-        const currentQ = this.engine.currentQuestion
-
-
-        this.dom.currentQuestionSpan.textContent = this.engine.currentIndex + 1
-        this.dom.questionText.textContent = currentQ.questionText
-        this.dom.progressBar.style.width = `${this.engine.progressPercent}%`
-
-
-
-        this.dom.answersContainer.innerHTML = ""
-
-        currentQ.answers.forEach(answer => {
-            const button = document.createElement("button")
-            button.textContent = answer.text
-            button.classList.add("answer-btn")
-            button.dataset.correct = answer.button
-
-            button.addEventListener("click", (e)=>{this.handleAnswer(e)})
-            this.dom.answersContainer.appendChild(button)
-        });
-    }
-
-
-    handleAnswer(event){
-        if (this.isLocked) return
-        this.isLocked = true
-
-        const selectedButton = event.target
-        const isCorrect = selectedButton.dataset.correct === "true"
-
-
-        this.engine.submitAnswer(isCorrect)
-
-        this.highlightAnswer(selectedButton)
-
-        if (isCorrect){
-            this.dom.scoreSpan.textContent = this.engine.scoreSpan
-        }
-
-
-        setTimeout(()=> {
-            this.engine.next()
-            if (this.engine.isEnded()){
-                this.showResults()
-            }else {
-                this.renderQuestion()
-            }
-        }, 500)
-
-    }
-
-
-    highlightAnswer(){
-        Array.from(this.dom.answersContainer.childred).forEach((button)=>{
-            if (button.dataset.correct === "true"){
-                button.classList.add("correct")
-            }
-            else if  (button === selectedButton){
-                button.classList.add("incorrect")
-            }
-        })
-    }
-
-
-    showResults(){
-        this.dom.quizScreen.classList.remove("active")
-        this.dom.startScreen.classList.remove("active")
-        this.dom.resultScreen.classList.remove("active")
-
-        this.finalScoreSpan.textContent = this.engine.score
-
-        const percentage = this.engine.progressPercent
-
-
-        if (percentage === 100) {
-        this.dom.resultMessage.textContent = "Fucking Great Job";
+    if (percentage === 100) {
+      this.dom.resultMessage.textContent = "Fucking Great Job";
     } else {
-        this.dom.resultMessage.textContent = "Not Fucking Great Job, Wanker";
+      this.dom.resultMessage.textContent = "Not Fucking Great Job, Wanker";
     }
-
-    
-    }
-
+  }
 }
