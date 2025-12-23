db6a54eabe3dc5314e2b9da32a57fae9b4cf54fe
Gliitchhh410 <ahmedmounir532@gmail.com>
2025-12-23 21:41:41 +0200

    Project 2 init

---
+++ Project 2/script.js
@@ -0,0 +1,37 @@
+const generateBtn = document.getElementById("generate-btn");
+const paletteContainer = document.querySelector(".paletter-container");
+
+generateBtn.addEventListener("click", generatePalette);
+
+generatePalette();
+
+function generatePalette() {
+  const colors = [];
+
+  for (let i = 0; i < 5; i++) {
+    colors.push(generateRandomColor());
+  }
+
+  updatePaletteUI(colors);
+}
+
+function updatePaletteUI(colors) {
+  const colorBoxes = document.querySelectorAll(".color-box");
+
+  colorBoxes.forEach((box, index) => {
+    const color = colors[index];
+    const colorDiv = box.querySelector(".color");
+    const hexDiv = box.querySelector(".hex-value");
+
+    colorDiv.style.backgroundColor = color;
+    hexDiv.textContent = color;
+  });
+}
+function generateRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
