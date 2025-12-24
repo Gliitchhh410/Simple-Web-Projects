9040d70d8d51fcb78794030220c3d9fccc6dd1e2
Gliitchhh410 <ahmedmounir532@gmail.com>
2025-12-24 18:18:41 +0200

    feat: PaletteEngine Class

---
+++ Project 2 - Better/src/models/PaletteEngine.js
@@ -0,0 +1,25 @@
+export class PaletteEngine{
+    constructor(size = 5){
+        this.size = size
+    }
+
+    generate(){
+        const colors = []
+        for (let i = 0; i<this.size; i++){
+            colors.push(this._getRandomHex())
+        }
+
+        return colors
+    }
+
+
+    _getRandomHex(){
+        const letters = "0123456789ABCDEF"
+        let color = "#"
+        for (let i = 0; i<this.size; i++){
+            color += letter[Math.floor(Math.random() * 16)]
+        }
+
+        return color
+    }
+}
