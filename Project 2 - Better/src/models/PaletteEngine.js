bc9f270c5a8a322a8ef9be42a0554bed183ffe20
Gliitchhh410 <ahmedmounir532@gmail.com>
2025-12-24 19:08:36 +0200

    fix: typo letter instead of letters

--- Project 2 - Better/src/models/PaletteEngine.js
+++ Project 2 - Better/src/models/PaletteEngine.js
@@ -17,7 +17,7 @@ export class PaletteEngine{
         const letters = "0123456789ABCDEF"
         let color = "#"
         for (let i = 0; i<this.size; i++){
-            color += letter[Math.floor(Math.random() * 16)]
+            color += letters[Math.floor(Math.random() * 16)]
         }
 
         return color
