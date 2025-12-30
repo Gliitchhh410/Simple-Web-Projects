33da8368e9d8c8ba28d23b572f2b2b69a20570ff
Gliitchhh410 <ahmedmounir532@gmail.com>
2025-12-30 23:59:15 +0200

    feat: drag drop features implementation

--- Project 3/script.js
+++ Project 3/script.js
@@ -1,16 +1,44 @@
-const cards = document.querySelectorAll(".card")
-const lists   = document.querySelectorAll(".list")
+const cards = document.querySelectorAll(".card");
+const lists = document.querySelectorAll(".list");
 
-
-for (const card of cards){
-    card.addEventListener("dragstart", dragStart)
-    card.addEventListener("drageEnd", dragEnd)
+for (const card of cards) {
+  card.addEventListener("dragstart", dragStart);
+  card.addEventListener("dragend", dragEnd);
 }
 
+for (const list of lists) {
+  list.addEventListener("dragover", dragOver);
+  list.addEventListener("dragenter", dragEnter);
+  list.addEventListener("dragleave", dragLeave);
+  list.addEventListener("drop", dragDrop);
+}
 
-for (const list of lists){
-    card.addEventListener("dragover", dragOver)
-    card.addEventListener("dragenter", dragEnter)
-    card.addEventListener("dragleave", dragLeave)
-    card.addEventListener("drop", drop)
+function dragStart(e) {
+  e.dataTransfer.setData("text/plain", this.id);
+}
+
+function dragEnd() {
+  console.log("Drag Ended");
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+
+  this.classList.add("over");
+}
+
+function dragLeave(e) {
+  this.classList.remove("over");
+}
+
+function dragDrop(e) {
+  const id = e.dataTransfer.getData("text/plain");
+  const card = document.getElementById(id);
+
+  this.appendChild(card);
+  this.classList.remove("over");
 }
